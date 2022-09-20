387e49a2956ddf49c59892c79adc26a3690f9783
ronanborja <ronanborja1205@gmail.com>
2022-09-20 18:33:06 +0800

    Exercise2 Angular

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FormComponent } from './components/form/form.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProfileComponent,
+    FormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
